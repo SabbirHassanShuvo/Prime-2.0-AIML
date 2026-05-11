467ecbc0b238878d2627e343dc7a31a6860affe3
SabbirHassanShuvo <sabbirhassanshuvo71@gmail.com>
2026-05-11 06:27:06 +0600

    Finshed Function

---
+++ Python/Function06.py
@@ -0,0 +1,12 @@
+# avg definition
+def avg(a, b):
+    return (a + b) / 2
+# avg call
+print(avg(10, 20))
+
+# check type of fuction
+name = "Sabbir Hassan"
+roll = 101
+print(type(name))
+print(type(roll))
+
