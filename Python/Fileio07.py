8ae8b342b46f2f784cd545d9ece539293862d2f4
SabbirHassanShuvo <sabbirhassanshuvo71@gmail.com>
2026-05-15 06:31:48 +0600

    Refactor file handling in Fileio07.py and add prektic.py for demo file operations

--- Python/Fileio07.py
+++ Python/Fileio07.py
@@ -5,3 +5,23 @@
 # print(type(data))
 # f.close()
 
+# write to file demo.txt
+# f = open("Python/demo.txt", "w")
+# f.write("This is a demo file.\n")
+
+# This will overwrite the existing content of the file. If you want to append to the file, use "a".
+# f = open("Python/demo.txt", "a")
+# f.write("This is this third line.\n")
+# f.close() 
+
+
+# with open statement
+# with open("Python/demo.txt", "r") as f:
+#     data = f.read()
+#     print(data)
+#     print(type(data))
+
+
+# Delete a file
+import os
+# os.remove("Python/demo.txt")
---
+++ Python/prektic.py
@@ -0,0 +1,4 @@
+# 
+with open("Python/demo.txt", "w") as f:
+    data = f.read()
+    print(data)
