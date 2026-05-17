3988577eba195ed1ad8df6ae185e82005a140995
SabbirHassanShuvo <sabbirhassanshuvo71@gmail.com>
2026-05-17 06:22:35 +0600

    Start Class and object

---
+++ Python/Class&Object09.py
@@ -0,0 +1,44 @@
+# class Car:
+#     color = "Red"
+#     brand = "Toyota"
+# car1 = Car()
+# print(car1.color)  # Output: Red
+# print(car1.brand)  # Output: Toyota
+
+
+# class Student:
+#     name = "John"
+#     age = 20
+# student1 = Student()
+# print(student1.name)  # Output: John
+# print(student1.age)   # Output: 20
+
+
+# class Student:
+#     name = "John"
+# student1 = Student()
+# print(student1.name)  # Output: John 
+
+
+# class on defind contructor
+# class Student:
+#     def __init__(self,name, age):
+#         self.name = name
+#         self.age = age
+
+# s1 = Student("Md Sabbir Hossain", 25)
+# print(s1.name, s1.age) 
+
+# s2 = Student("Md Rakib Hossain", 50)
+# print(s2.name, s2.age)
+
+# class and obejct attr
+class Student:
+    collefe
+
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+    
+s1 = Student("Md Sabbir hassan", 50)
+print(s1.name)
